c947992df8ce35d06a924870bb8b8cda697e3dc2
ArtiMak1984 <artempop25@mail.ru>
2024-03-03 21:19:43 +0300

    2

--- app/config.py
+++ app/config.py
@@ -4,4 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 
+
 TOKEN_TELEGRAM = os.getenv("TOKEN_TELEGRAM")
+
+TOKEN_OPENAI = os.getenv("TOKEN_OPENAI")
--- app/main.py
+++ app/main.py
@@ -1,16 +1,37 @@
 import telebot
 
-from config import TOKEN_TELEGRAM
+from config import TOKEN_TELEGRAM, TOKEN_OPENAI
+from openai import OpenAI
 
-bot = telebot.TeleBot(TOKEN_TELEGRAM, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+client = OpenAI(
+    api_key=TOKEN_OPENAI,
+)
 
-@bot.message_handler(commands=['start', 'help'])
+
+bot = telebot.TeleBot(TOKEN_TELEGRAM, parse_mode=None)
+
+
+@bot.message_handler(commands=['start', 'help', 'asd'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-	
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
 @bot.message_handler(func=lambda m: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
-	
+def answer_all(message):
+    print(message.from_user)
+    completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": message.text,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    result = completion.choices[0].message.content
+    bot.reply_to(message, result)
+
+
 if __name__ == "__main__":
     bot.polling()
+    
